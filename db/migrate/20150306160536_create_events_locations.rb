37adbd20edbabace49448d0a0303cb5da731be3f
guymon.hall <guymon.hall@gmail.com>
2015-03-06 09:25:43 -0800

    CS 270, Spring 2015, QR Code Scavenger Hunt, Phase 2

--- db/migrate/20150306160536_create_events_locations.rb
+++ db/migrate/20150306160536_create_events_locations.rb
@@ -1,8 +1,8 @@
 class CreateEventsLocations < ActiveRecord::Migration
   def change
     create_table :events_locations, id: false do |t|
-        t.belongs_to :locations, index:true
-        t.belongs_to :events, index:true
+        t.belongs_to :location, index:true
+        t.belongs_to :event, index:true
     end
   end
 end
